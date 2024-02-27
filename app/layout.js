813783cb8489979779bb7f8a9639933fbde22e11
ericodwr <ericodr@upnvj.ac.id>
2024-02-27 15:12:32 +0700

    fixing seo

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
           content="Erico Dwi Rosadi | Front End Developer"
         />
         <meta name="robots" content="max-image-preview:large" />
-        <link rel="canonical" href="https://mitralancar.com/" />
+        <link rel="canonical" href="https://www.ericodwr.space/" />
         <meta property="og:locale" content="en_US" />
         <meta
           property="og:site_name"
@@ -32,15 +32,7 @@ export default function RootLayout({ children }) {
           property="og:description"
           content="Erico Dwi Rosadi | Front End Developer"
         />
-        <meta property="og:url" content="https://mitralancar.com/" />
-        <meta
-          property="article:published_time"
-          content="2022-01-23T23:22:10+00:00"
-        />
-        <meta
-          property="article:modified_time"
-          content="2022-01-29T15:42:02+00:00"
-        />
+        <meta property="og:url" content="https://www.ericodwr.space/" />
         <meta name="twitter:card" content="summary" />
         <meta
           name="twitter:title"
